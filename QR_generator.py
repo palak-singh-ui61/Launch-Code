627dfdc10d841f2fd79e99422a79aafca162c631
palak-singh-ui61 <thakurpalak709@gmail.com>
2024-10-17 20:23:07 +0530

    Add files via upload

---
+++ QR_generator.py
@@ -0,0 +1,23 @@
+import qrcode
+upi_id=input("Enetr Your UPI ID = : ")
+phonepe_url=f"upi://pay?pa={upi_id}&pn=Recipient%20Name&mc=1234"
+paytm_url=f"upi://pay?pa={upi_id}&pn=Recipient%20Name&mc=1234"
+google_pay_url=f"upi://pay?pa={upi_id}&pn=Recipient%20Name&mc=1234"
+
+#.......create QR code.............
+phonepe_qr=qrcode.make(phonepe_url)
+paytm_qr=qrcode.make(paytm_url)
+google_pay_qr=qrcode.make(google_pay_url)
+
+#.....save QR.....
+phonepe_qr.save('phonepe_qr.png')
+paytm_qr.save('paytm_qr.png')
+google_pay_qr.save('google_pay_qr.png')
+
+#....Display QR.........
+phonepe_qr.show()
+paytm_qr.show()
+google_pay_qr.show()
+
+
+
